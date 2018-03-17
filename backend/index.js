0210749ccd3d8d7b1d0ccef26ff3752fcdad919d
markus <marcholio09@gmail.com>
2018-03-17 23:27:29 +0800

    Cars added and also custom icons for markers

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,66 @@ const minLon = 114.161;
 const maxLat = 22.336;
 const maxLon = 114.192183;
 
+const carsAvailable = 10;
+
+// Random coordinate functions
+const randomLat = () => Math.random() * (maxLat - minLat) + minLat;
+const randomLon = () => Math.random() * (maxLon - minLon) + minLon;
+
+const toRadians = deg => deg * Math.PI / 180;
+
+// Get distance between coordinates
+const distance = (a, b) => {
+  const lat1 = toRadians(a.lat);
+  const lat2 = toRadians(b.lat);
+  const lon1 = toRadians(a.lon);
+  const lon2 = toRadians(b.lon);
+
+  const dLat = lat2 - lat1;
+  const dLon = lon2 - lon1;
+
+  const x =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const y = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x));
+
+  return 6371e3 * y;
+};
+
 /*
  * Returns a new request from customer to get a ride
  */
 const getRideRequest = () => {
-  const lat = Math.random() * (maxLat - minLat) + minLat;
-  const lon = Math.random() * (maxLon - minLon) + minLon;
-  return { eventType: 'rideRequest', lat, lon };
+  const latF = randomLat();
+  const lonF = randomLon();
+  let latT = randomLat();
+  let lonT = randomLon();
+  while (distance({ lat: latF, lon: lonF }, { lat: latT, lon: lonT }) < 1000) {
+    latT = randomLat();
+    lonT = randomLon();
+  }
+
+  return {
+    eventType: 'rideRequest',
+    from: { lat: latF, lon: lonF },
+    to: { lat: latT, lon: lonT },
+  };
 };
+
+const getCar = () => {
+  const lat = randomLat();
+  const lon = randomLon();
+  return { position: { lat, lon }, available: true };
+};
+
+wss.on('connection', client => {
+  client.send(
+    JSON.stringify({
+      eventType: 'initialCars',
+      cars: new Array(carsAvailable).fill(null).map(() => getCar()),
+    })
+  );
+});
 
 // Main loop for mocking ride requests
 setInterval(
--- robo-taxi-web/src/Map.js
+++ robo-taxi-web/src/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 import mapStyles from './MapStyles';
+import customerIcon from './icons/customer.png';
+import carAvailableIcon from './icons/car_free.png';
+import carReservedIcon from './icons/car_reserved.png';
 
 // eslint-disable-next-line no-undef
 const apiKey = process.env.REACT_APP_MAPS_API_KEY;
@@ -10,7 +13,7 @@ const center = { lat: 22.3185, lng: 114.1767936 };
 class MapWrapper extends Component {
   constructor() {
     super();
-    this.state = { rideRequests: [] };
+    this.state = { rideRequests: [], cars: [] };
   }
 
   componentWillMount() {
@@ -21,25 +24,47 @@ class MapWrapper extends Component {
 
       if (data.eventType === 'rideRequest') {
         this.addRideRequest(data);
+      } else if (data.eventType === 'initialCars') {
+        this.setState({ cars: data.cars });
       }
     };
   }
 
   // Adds new request for ride to state to add a marker to map
-  addRideRequest({ lat, lon }) {
+  addRideRequest({ from }) {
+    const { lat, lon } = from;
     this.setState({
       rideRequests: this.state.rideRequests.concat([{ lat, lon }]),
     });
   }
 
   render() {
-    return (
+    return this.props.google ? (
       <Map google={this.props.google} initialCenter={center} styles={mapStyles}>
         {this.state.rideRequests.map(r => (
-          <Marker key={r.lat} position={{ lat: r.lat, lng: r.lon }} />
+          <Marker
+            key={r.lat}
+            position={{ lat: r.lat, lng: r.lon }}
+            icon={{
+              url: customerIcon,
+              anchor: new this.props.google.maps.Point(8, 8),
+              scaledSize: new this.props.google.maps.Size(8, 8),
+            }}
+          />
+        ))}
+        {this.state.cars.map(c => (
+          <Marker
+            key={c.position.lat}
+            position={{ lat: c.position.lat, lng: c.position.lon }}
+            icon={{
+              url: c.available ? carAvailableIcon : carReservedIcon,
+              anchor: new this.props.google.maps.Point(8, 8),
+              scaledSize: new this.props.google.maps.Size(8, 8),
+            }}
+          />
         ))}
       </Map>
-    );
+    ) : null;
   }
 }
 
